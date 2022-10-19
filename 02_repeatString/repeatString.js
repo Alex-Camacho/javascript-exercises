448c7c49c5d5a28a5080d4e0998e34029eeb9341
Alex-Camacho <cuentascriptico@gmail.com>
2022-10-19 13:29:03 +0200

    Ejercicios 1 y 2 hechos

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(string,number) {
+    let stringContainer = "";
+    if(number <0) return "ERROR";
+    for(i=1; i <= number; i++){
+        
+        stringContainer += `${string}`;
+    } return stringContainer;
 
 };
 
