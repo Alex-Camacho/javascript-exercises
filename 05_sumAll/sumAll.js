926f364a6c5366d31517b60501c9c97b1daae5df
Alex-Camacho <cuentascriptico@gmail.com>
2022-10-20 12:13:13 +0200

    Excercise done! using loop i=a sum+=i until b

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,22 @@
-const sumAll = function() {
-
+const sumAll = function(a,b) {
+    let sum = 0;
+    if(a < 0 || b < 0 || isNaN(a) || isNaN(b) || typeof a === 'string' || typeof b === 'string' ){
+        return "ERROR";
+    }
+    else if(a > b){
+        for(i = b; i <= a; i++){
+            sum += i;
+        }  return sum;
+    }
+    
+    else {for(i = a; i <= b; i++){
+        sum += i;
+    }
+    return sum;
+    }
 };
 
 // Do not edit below this line
 module.exports = sumAll;
+
+
