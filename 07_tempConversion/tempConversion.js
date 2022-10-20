f8a93db724ad931214700b247d069e99bf16c40b
Alex-Camacho <cuentascriptico@gmail.com>
2022-10-20 20:00:17 +0200

    Excercise done.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,15 @@
-const ftoc = function() {
-
+const ftoc = function(farenh) {
+  let celc = (farenh - 32) * (5/9);
+  Number(celc);
+  celc = Math.round(celc *10) / 10;
+  return celc;
 };
 
-const ctof = function() {
-
+const ctof = function(celc) {
+  let farenh = (celc * (9/5) +32);
+  Number(farenh);
+  farenh = Math.round(farenh *10) / 10;
+  return farenh;
 };
 
 // Do not edit below this line
