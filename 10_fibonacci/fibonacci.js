dd722f40281f64af4e5cfb46fb17deafddeb85c1
Alex-Camacho <cuentascriptico@gmail.com>
2022-10-31 22:33:42 +0100

    Excercise done

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    if(a < 0){
+        return "OOPS";
+    }
+    let fibo = [1, 1];
+    let number;
+    for(i = 1; i < 50; i++){
+        number = fibo[i] + fibo[i - 1];
+        fibo.push(number);
+    }
+    return fibo[a - 1];
 };
 
 // Do not edit below this line
