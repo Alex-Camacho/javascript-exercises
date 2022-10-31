6b688d3242f95e0cd3a79513ef43b2de7421c523
Alex-Camacho <cuentascriptico@gmail.com>
2022-10-31 23:33:17 +0100

    Excercise done!

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,16 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(object) {
+    const currentYear = (new Date()).getFullYear();
+    for (i = 0; i < Array.length; i++){
+    if (object[i].yearOfDeath === undefined){
+        object[i].yearOfDeath = currentYear;
+        };
+    };
+     const oldest = object.sort((a,b) => (a.yearOfDeath - a.yearOfBirth) >
+     (b.yearOfDeath - b.yearOfBirth) ? -1 : 1);
+     return oldest[0];
 };
 
 // Do not edit below this line
 module.exports = findTheOldest;
+
+
