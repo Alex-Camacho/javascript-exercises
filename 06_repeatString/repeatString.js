9c2784c60a2d7b09d7636f4dbd07b0499648e2b1
Alex-Camacho <cuentascriptico@gmail.com>
2025-09-07 23:45:49 +0200

    Ejercicio realizado

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    finalString = string;
+    if(num === 0){
+        finalString = '';
+    }
+    else if(num < 0){
+        finalString = "ERROR";
+    }
+    for(i = 1; i < num; i++){
+        finalString = finalString + string;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
